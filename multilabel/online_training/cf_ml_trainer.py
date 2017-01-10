9a19ec993431ba2978f1a2786740da59e1898588
Praveen <praveen049@yahoo.com>
2017-01-10 07:51:11 +0000

    Verified the exercise prediction

--- multilabel/online_training/cf_ml_trainer.py
+++ multilabel/online_training/cf_ml_trainer.py
@@ -20,44 +20,9 @@ X_train = np.array(training_data.workout)
 l_train = np.array(training_data[['e1','e2','e3','e4','e5','e6']], dtype=str)
 
 Y = lb.fit_transform(l_train)
-print Y
+#print l_train
+#print Y
 
-exercise_train_text = [
-                ["lifting", "gymnastics"],  #1
-		["lifting"], #2
-		["lifting", "gymnastics" , "calisthenics"], #3
-                ["lifting","gymnastics"], #4
-		["calisthenics", "kettlebell"], #5
-		["calisthenics","gymnastics"], #6
-                ["calisthenics","gymnastics"], #7
-		["calisthenics","gymnastics"], #8
-		["calisthenics","gymnastics"], #9 
-                ["lifting", "calisthenics"],  #10
-		["lifting", "gymnastics"], #11
-		["lifting", "gymnastics"], #12
-                ["lifting"], #13
-		["lifting","gymnastics"], #14
-		["calisthenics"], #15
-                ["calisthenics","lifting"], #16
-		["lifting"], #17
-                ["calisthenics"], #18
-		["kettlebell"], #19
-		["calisthenics", "plyometrics","gymnastics"], #20
-                ["lifting","gymnastics"], #21
-		["kettlebell","calisthenics"], #22
-		["lifting", "plyometrics","calisthenics"], #23
-                ["lifting", "kettlebell", "plyometrics"], #24
-		["plyometrics"],  #25
-		["kettlebell"], #26
-                ["lifting"], #27
-		["kettlebell"], #28
-		["calisthenics"], #29
-                ["lifting"],  #30
-		["lifting", "plyometrics", "gymnastics"], #31
-		["lifting","calisthenics","plyometrics"] #32
-                ]
-
-                
 if (X_train.size != len(l_train)):
     print "Training sample size %d, Exercise Labels %d Do not match" % ( X_train.size, len(l_train))
     sys.exit()    
